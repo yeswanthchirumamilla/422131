56779060c1f8169098be6a0d4ebe42674a693438
yesh <yesh@yesh.myguest.virtualbox.org>
2024-04-14 09:39:22 +0530

    initial commi:add

---
+++ git.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+git init
+
+ls 
+
+git add .
+
+git commit -m "Initial commit: Adding assignments"
+
+git checkout -b v1
+
+mkdir assign6&7
+
+mv first.sh second.sh cmd.sh git.sh assign6&7
+
+mkdir tfiles
+
+mv file1.txt file2.txt output.txt tfiles
+
+git add .a
+
+git commit -m "v1:updating directories"
+
+git checkout master
+
+git merge v1
+
+git remote add origin
+
+git push -u origin master
